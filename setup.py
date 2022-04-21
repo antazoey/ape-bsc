6212ae8c37eb8d7a4401be72f4a92852f9968d66
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-04-21 03:10:55 -0700

    fix: eth-ape version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "importlib-metadata ; python_version<'3.8'",
-        "eth-ape>=0.1,<0.2",
+        "eth-ape>=0.2.1,<0.3.0",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7.2,<3.11",
     extras_require=extras_require,
